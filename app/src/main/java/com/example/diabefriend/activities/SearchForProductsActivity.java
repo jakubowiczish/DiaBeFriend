4a9c65d174f3caca562fd00a0bdff012251a63b0
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-02-23 18:05:59 +0100

    Add adapter and csv file for autoCompleteTextView in SearchForProductsActivity

--- app/src/main/java/com/example/diabefriend/activities/SearchForProductsActivity.java
+++ app/src/main/java/com/example/diabefriend/activities/SearchForProductsActivity.java
@@ -2,17 +2,28 @@ package com.example.diabefriend.activities;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import com.example.diabefriend.model.Product;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import com.example.diabefriend.R;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchForProductsActivity extends AppCompatActivity {
+
+    private List<Product> products = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +32,58 @@ public class SearchForProductsActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        readProductsData();
+
+        List<String> productNames = getProductNames();
+
+        AutoCompleteTextView autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                productNames
+        );
+
+        autoCompleteTextView.setAdapter(adapter);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+
+    private List<String> getProductNames() {
+        List<String> productNames = new ArrayList<>();
+        for (Product product : products) {
+            productNames.add(product.getName());
+        }
+        return productNames;
+    }
+
+    private void readProductsData() {
+        InputStream inputStream = getResources().openRawResource(R.raw.productsdata);
+        BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(inputStream, Charset.forName("UTF-8"))
+        );
+
+        String line = "";
+        try {
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] tokens = line.split(";");
+                Product product = new Product(
+                        tokens[0],
+                        Float.parseFloat(tokens[1]),
+                        Float.parseFloat(tokens[2]),
+                        Float.parseFloat(tokens[3]),
+                        Float.parseFloat(tokens[4]),
+                        Float.parseFloat(tokens[5])
+                );
+                products.add(product);
+
+                Log.d("Product created", "New product has just been created: " + product);
+            }
+        } catch (IOException e) {
+            Log.wtf("SearchForProductsActivity", "Error while reading data from file on line" + line, e);
+            e.printStackTrace();
+        }
+    }
+
 
 }
---
+++ app/src/main/java/com/example/diabefriend/model/Product.java
@@ -0,0 +1,23 @@
+package com.example.diabefriend.model;
+
+public class Product {
+    private String name;
+    private float weight;
+    private float kCal;
+    private float fat;
+    private float carbohydrates;
+    private float proteins;
+
+    public Product(String name, float weight, float kCal, float fat, float carbohydrates, float proteins) {
+        this.name = name;
+        this.weight = weight;
+        this.kCal = kCal;
+        this.fat = fat;
+        this.carbohydrates = carbohydrates;
+        this.proteins = proteins;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
